11272463d312fe4867b4045f038a734d36d97b32
jklua <yulia.lukashevich@gmail.com>
2020-10-25 22:47:08 +0200

    updated25oct

--- src/test/java/com/ilcarro/qa/tests/TestBase.java
+++ src/test/java/com/ilcarro/qa/tests/TestBase.java
@@ -2,12 +2,13 @@ package com.ilcarro.qa.tests;
 
 import com.ilcarro.qa.framework.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.*;
 
 public class TestBase {
 
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
-
+Logger logger= LoggerFactory.getLogger(TestBase.class);
     @BeforeSuite
     public void setUp(){
         app.init();
@@ -15,8 +16,15 @@ public class TestBase {
 
     }
 
+@BeforeMethod
+public void startTest(){
+        logger.info("Start test");
+}
 
-
+@AfterMethod
+public void stopTest(){
+        logger.info("Stop test");
+}
     @AfterSuite(enabled = true)
     public void tearDown(){
         app.stop();
